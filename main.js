c8b5caa7139ba2eefa2803b9e23adfd033ea9d03
Jett <jettburns11@icloud.com>
2017-11-06 09:49:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Write code here, obviously
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+
 const prefix = '?';
 const bannedRoles = [];
 //var blacklist = require("./bot.json");
