359c20d7159288c1a78ec625d323173ae0fd940a
Jett <jettburns11@icloud.com>
2017-11-17 21:52:32 -0600

    Update main.js

--- main.js
+++ main.js
@@ -152,7 +152,7 @@ client.on("messageReactionAdd", (messageReaction, user) => {
             let embed = new Discord.RichEmbed();
             embed.setColor([247, 237, 96]);
             embed.setAuthor(messageReaction.message.author.tag, messageReaction.message.author.avatarURL);
-            embed.addField(`Message flagged in ${messageReaction.message.channel.name} by <someone>`, messageReaction.message.content);
+            embed.addField(`Message flagged in #${messageReaction.message.channel.name} by <someone>`, messageReaction.message.content);
             // embed.addField('Flagged by:', 'WIP');
             // messageReaction.message.channel.send({ embed }); // nah
             client.channels.find('id', '369502585440436236').send({ embed });
