d2e566696a1127bf4397b70162c339c7fd2f4c3a
Jett <jettburns11@icloud.com>
2017-12-08 21:11:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,17 +13,15 @@ const whitelistRoles = ['Trusty flagger'];
 
 const getDefaultChannel = async (guild) => {
 
-    if(guild.channel.has(guild.id))
+    if(guild.channels.has(guild.id))
         return guild.channels.get(guild.id)
 
     if(guild.channels.exists("name", "general"))
         return guild.channels.find("name", "general");
-    return guild.channels
-    .filter(c => c.type === "text" &&
-        c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-   .sort((a, b) => a.position - b.position ||
-        Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-   .first();
+    return guild.channels.filter(c => c.type === "text" && 
+                                 c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+        .sort((a, b) => a.position - b.position || 
+              Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber()).first();
 }
 
 const commands = {
