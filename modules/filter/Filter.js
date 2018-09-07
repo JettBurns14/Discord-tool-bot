c4956a3feff125358c127f76f6cb76709dc016d2
Ethan Luis McDonough <elmcdonough@comcast.net>
2018-09-07 14:43:10 -0500

    Remove emoji filter entirely
    That was a bad idea in retrospect.

--- modules/filter/Filter.js
+++ modules/filter/Filter.js
@@ -1,30 +1,21 @@
 const SizedQueue = require("./SizedQueue");
 const Homoglyphs = require("./Homoglyphs");
 const { Colors } = require("../../constants");
-const onlyEmoji = require('emoji-aware').onlyEmoji;
 
 // Strip unwanted characters that could be used for evading the filter
 const removeSymbols = str =>
     str.replace(/[~\!@#$%^&*()-=_+\[\]{}|"";:\/?.>,<`]/g, "");
-
-// Replace server emotes with regular emojis so that `onlyEmoji` picks them up
-const replaceServerEmotes = str => str.replace(/<:\w+:\d+>/g, "\uD83D\uDC3E");
-
-// Calculate what percentage of chars are emojis.
-// Returns a value between 0 and 1, 0 being no emojis and 1 being completely emojis
-const calculateEmojiFrequency = str => onlyEmoji(str).length / [...str].length;
 
 const filterIcon = "https://cdn.discordapp.com/attachments/306119383820795904/480069533676208130/emoji.png";
 
 class Filter {
     constructor(rawGlyphs, options) {
         this.homoglyphs = Homoglyphs.load(rawGlyphs);
-        this.profanity = options.profanity;
+        this.profanity = options.profanity.map(word => word.toLowerCase().trim());
         this.chain = {
             inner: new SizedQueue(options.queueSize || 0),
             threshold: options.threshold,
         };
-        this.emoteFrequency = options.emoteFrequency || 0.0;
     }
 
     // Checks if the a cleaned message is "good" (meaning that it should not get filtered)
@@ -32,13 +23,12 @@ class Filter {
         const cleanedWords = cleanMessage.split(/\s+/);
 
         return cleanedWords.every(word => !this.profanity.includes(word.toLowerCase())) &&
-            this.chain.inner.countInstances(cleanMessage) < this.chain.threshold &&
-            calculateEmojiFrequency(cleanMessage) < this.emoteFrequency;
+            this.chain.inner.countInstances(cleanMessage) < this.chain.threshold;
     }
 
     // Strips symbols and homoglyphs from a message
     cleanMessage(msg) {
-        return removeSymbols(this.homoglyphs.replace(replaceServerEmotes(msg)));
+        return removeSymbols(this.homoglyphs.replace(msg));
     }
 
     // The main filter method.  Returns a promise
